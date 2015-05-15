3d661210512e0d2ec72b6b7496288ffd4809dc39
travism26 <travismartin266@gmail.com>
2015-05-14 23:57:41 -0300

    comment repo interface finished

--- src/org/travis/blogExample/CommentRepoInterface.java
+++ src/org/travis/blogExample/CommentRepoInterface.java
@@ -2,4 +2,17 @@ package org.travis.blogExample;
 
 public interface CommentRepoInterface {
 
+	public String createComment(int id, String comment);
+
+	public String getComment(int id);
+	
+	public String updateComment(int id, String comment);
+	
+	public String deleteComment(int id, String comment);
+	
 }
+
+//side notes:
+//each comment belongs to a Article
+//1 to many relationship. aka an article can have many comments
+//AND a comment can only belong to 1 Article (not necessarily but lets keep it simple)
