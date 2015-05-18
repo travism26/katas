adb3b040ee8ba882b63e44ce1fde2e830153f2c2
travism26 <travismartin266@gmail.com>
2015-05-18 12:06:30 -0300

    moved gradle file to root

---
+++ src/org/travis/blogExample/blogrunner.java
@@ -0,0 +1,19 @@
+package org.travis.blogExample;
+
+import java.util.ArrayList;
+
+import org.travis.blogExample.Article.Article;
+import org.travis.blogExample.Comment.Comment;
+
+public class blogrunner {
+	
+	public static void main(String[] args)
+	{
+		ArrayList<Article> articles;
+		ArrayList<Comment> comments;
+		
+		
+	}
+	
+	
+}
